02dc2ea046774970f99b5e8c9d527b89ba5f773a
viniciusccosta <viniciusccosta95@gmail.com>
2025-04-24 15:21:02 -0300

    Refactor background task logic; improve error handling and streamline file reading and data sending processes.

--- topsoft/tasks.py
+++ topsoft/tasks.py
@@ -11,6 +11,8 @@ def background_task(stop_event):
         """
         Read the bilhetes file and return a list of dictionaries with the data.
         """
+        logging.debug(f"Reading bilhetes file: {filepath}")
+
         with open(filepath, "r") as f:
             data = f.readlines()
 
@@ -26,43 +28,48 @@ def background_task(stop_event):
 
         api_key = get_api_key()
 
-    # Log the start of the task
-    logger.info("Starting background task")
+        return True  # TODO: Simulating success
 
-    # Variables
-    intervalo = get_interval()
-    cnt = 1
+    def wait_for_interval(stop_event):
+        """
+        Wait for the specified interval.
+        """
 
-    # Main loop
-    while True:
-        logger.info(f"Running background task {cnt}")
-        cnt += 1
+        intervalo = get_interval()
 
-        # Ler arquivo (conforme configuração)
-        try:
-            data = read_bilhetes_file(get_bilhetes_path())
-        except Exception as e:
-            logger.error(f"Error reading bilhetes file: {e}")
-            logger.exception(e)
-            continue
-
-        # Enviar dados para ActivitySoft
-        try:
-            result = send_data_to_activitysoft(data)
-            if result:
-                logger.info("Data sent successfully")
-            else:
-                logger.error("Failed to send data")
-        except Exception as e:
-            logger.error(f"Error sending data to ActivitySoft: {e}")
-            logger.exception(e)
-
-        # TODO: Atualizar interface gráfica
-
-        # Wait for the next interval
         for i in range(intervalo):
             if stop_event.is_set():
                 logger.info("Stopping background task")
                 return
             logger.debug(f"Waiting for {intervalo - i} seconds")
             sleep(1)
+
+    # Log the start of the task
+    logger.info("Starting background task")
+
+    # Variables
+    cnt = 1
+
+    # Main loop
+    while not stop_event.is_set():
+        logger.info(f"Running background task {cnt}")
+        cnt += 1
+
+        try:
+            # Ler arquivo:
+            if bilhetes_path := get_bilhetes_path():
+                data = read_bilhetes_file(bilhetes_path)
+
+                # Enviar dados para ActivitySoft:
+                result = send_data_to_activitysoft(data)
+                if result:
+                    logger.info("Data sent successfully")
+                else:
+                    logger.error("Failed to send data")
+
+                # TODO: Atualizar interface gráfica
+        except Exception as e:
+            logger.error(f"Error na execução da tarefa")
+            logger.exception(e)
+        finally:
+            wait_for_interval(stop_event)
