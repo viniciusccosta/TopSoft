69e31d2dcf259320967929f3611a24ab78312067
viniciusccosta <viniciusccosta95@gmail.com>
2025-04-25 23:58:53 -0300

    Refactor constants to read from environment variables using decouple

--- topsoft/constants.py
+++ topsoft/constants.py
@@ -1,6 +1,8 @@
-SERVICE = "topsoft"  # TODO: Read from .env
-ACCOUNT = "activitysoft_api_key"  # TODO: Read from .env
+from decouple import config
 
-MIN_INTERVAL = 60  # TODO: Read from .env
-MAX_INTERVAL = 86400  # TODO: Read from .env
-DEFAULT_INTERVAL = 60  # TODO: Read from .env
+SERVICE = config("SERVICE", default="topsoft")
+ACCOUNT = config("ACCOUNT", default="activitysoft_api_key")
+
+MIN_INTERVAL = config("MIN_INTERVAL", default=60, cast=int)
+MAX_INTERVAL = config("MAX_INTERVAL", default=86400, cast=int)
+DEFAULT_INTERVAL = config("DEFAULT_INTERVAL", default=60, cast=int)
