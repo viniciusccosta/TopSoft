e71cc37fb20e497e5ceba00b62b4378a2366d313
viniciusccosta <viniciusccosta95@gmail.com>
2025-04-26 00:39:35 -0300

    Add ACTIVITY_URL configuration to constants for API endpoint

--- topsoft/constants.py
+++ topsoft/constants.py
@@ -6,3 +6,8 @@ ACCOUNT = config("ACCOUNT", default="activitysoft_api_key")
 MIN_INTERVAL = config("MIN_INTERVAL", default=60, cast=int)
 MAX_INTERVAL = config("MAX_INTERVAL", default=86400, cast=int)
 DEFAULT_INTERVAL = config("DEFAULT_INTERVAL", default=60, cast=int)
+
+ACTIVITY_URL = config(
+    "ACTIVITY_URL",
+    default=r"https://siga.activesoft.com.br/api/v0/marcar_frequencia_aluno/",
+)
