aa10be5822ba7425ee612bb9cb17d601b96022ab
viniciusccosta <viniciusccosta95@gmail.com>
2025-05-02 14:00:14 -0300

    Refactor utils.py to improve logging and enhance version retrieval from pyproject.toml

--- topsoft/utils.py
+++ topsoft/utils.py
@@ -1,5 +1,10 @@
+import logging
 import sys
 from os import path
+
+import toml
+
+logger = logging.getLogger(__name__)
 
 
 def process_batch(batch):
@@ -25,3 +30,19 @@ def get_path(filename):
     if hasattr(sys, "_MEIPASS"):
         return path.realpath(path.join(sys._MEIPASS, filename))
     return filename
+
+
+def get_current_version():
+    """
+    Get the current version of the application from pyproject.toml.
+    """
+    try:
+        with open("pyproject.toml", "r", encoding="utf-8") as f:
+            pyproject_data = toml.load(f)
+            return pyproject_data.get("project", {}).get("version", "0.0.0")
+    except FileNotFoundError:
+        logger.error("pyproject.toml not found")
+    except Exception as e:
+        logger.error(f"Error reading pyproject.toml: {e}")
+
+    return "0.0.0"
